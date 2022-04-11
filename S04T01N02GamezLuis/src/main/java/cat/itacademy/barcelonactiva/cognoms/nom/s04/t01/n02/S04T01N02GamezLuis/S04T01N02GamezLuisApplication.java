ca36f64ca35fbf7059d2e6deca60ec501b07a34f
LuisEGamez <luisgamezdvp@gmail.com>
2022-04-11 18:24:49 +0200

    First project with Spring Boot and Gradle

---
+++ S04T01N02GamezLuis/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n02/S04T01N02GamezLuis/S04T01N02GamezLuisApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.cognoms.nom.s04.t01.n02.S04T01N02GamezLuis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N02GamezLuisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N02GamezLuisApplication.class, args);
+	}
+
+}
---
+++ S04T01N02GamezLuis/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n02/S04T01N02GamezLuis/controllers/HelloWorldController.java
@@ -0,0 +1,31 @@
+package cat.itacademy.barcelonactiva.cognoms.nom.s04.t01.n02.S04T01N02GamezLuis.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController // Indicamos que esta clase será un controlador
+public class HelloWorldController {
+
+    @RequestMapping(value = "HelloWorld", method = RequestMethod.GET) //Indicamos la URL que debe devolver este contenido
+    public String saluda(@RequestParam(defaultValue = "UNKNOWN") String name){
+
+        return "Hola " + name + ". Estas ejecutando un proyecto Gradle";
+    }
+
+    @RequestMapping(value = {"HelloWorld2" , "HelloWorld2/{name}"}, method = RequestMethod.GET) //Indicamos las URL que debe devolver este contenido
+    public String saluda2(@PathVariable Optional<String> name){ // @PathVariable indica el parámetro que variará
+
+        String result;
+
+        if (name.isPresent()){
+            result = "Hola " + name.get() + ". Estas ejecutando un proyecto Gradle";
+        }else {
+            result = "Hola. Estas ejecutando un proyecto Gradle";
+        }
+
+        return result;
+    }
+
+
+}
