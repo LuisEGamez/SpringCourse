a8f2b67593e733c4c15917056fab52c148154c97
LuisEGamez <luisgamezdvp@gmail.com>
2022-04-12 22:55:01 +0200

    Creating project structure

---
+++ S04T02N01GamezLuis/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t02/n01/S04T02N01GamezLuis/model/domain/Fruit.java
@@ -0,0 +1,11 @@
+package cat.itacademy.barcelonactiva.cognoms.nom.s04.t02.n01.S04T02N01GamezLuis.model.domain;
+
+
+public class Fruit {
+
+    private long id;
+    private String name;
+    private int quantityKg;
+
+
+}
