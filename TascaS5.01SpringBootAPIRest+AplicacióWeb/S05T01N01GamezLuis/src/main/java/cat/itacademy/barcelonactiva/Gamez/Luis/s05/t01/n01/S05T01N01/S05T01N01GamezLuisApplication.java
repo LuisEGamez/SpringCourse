57218882bdcea4a8fd873be2ffdd62105c127cbf
LuisEGamez <luisgamezdvp@gmail.com>
2022-04-19 17:09:16 +0200

    Starting  project

---
+++ TascaS5.01SpringBootAPIRest+AplicacióWeb/S05T01N01GamezLuis/src/main/java/cat/itacademy/barcelonactiva/Gamez/Luis/s05/t01/n01/S05T01N01/S05T01N01GamezLuisApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.Gamez.Luis.s05.t01.n01.S05T01N01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S05T01N01GamezLuisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S05T01N01GamezLuisApplication.class, args);
+	}
+
+}
---
+++ TascaS5.01SpringBootAPIRest+AplicacióWeb/S05T01N01GamezLuis/src/test/java/cat/itacademy/barcelonactiva/Gamez/Luis/s05/t01/n01/S05T01N01/S05T01N01GamezLuisApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.Gamez.Luis.s05.t01.n01.S05T01N01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S05T01N01GamezLuisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
