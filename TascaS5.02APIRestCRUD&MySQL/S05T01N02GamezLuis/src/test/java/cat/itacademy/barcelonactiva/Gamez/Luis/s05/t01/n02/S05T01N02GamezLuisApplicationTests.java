ae50d44162c16760a83a716aa0caa5e7b7abd1f4
LuisEGamez <luisgamezdvp@gmail.com>
2022-04-26 18:12:08 +0200

    Adding dependencies

---
+++ TascaS5.02APIRestCRUD&MySQL/S05T01N02GamezLuis/src/test/java/cat/itacademy/barcelonactiva/Gamez/Luis/s05/t01/n02/S05T01N02GamezLuisApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.Gamez.Luis.s05.t01.n02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S05T01N02GamezLuisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
