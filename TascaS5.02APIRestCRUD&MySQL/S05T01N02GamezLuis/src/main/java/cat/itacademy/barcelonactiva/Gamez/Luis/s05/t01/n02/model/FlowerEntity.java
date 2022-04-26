f87b5792d4122b5b31f1d5ac98dbc892e862ba86
LuisEGamez <luisgamezdvp@gmail.com>
2022-04-26 18:20:59 +0200

    Adding flowerEntity

---
+++ TascaS5.02APIRestCRUD&MySQL/S05T01N02GamezLuis/src/main/java/cat/itacademy/barcelonactiva/Gamez/Luis/s05/t01/n02/model/FlowerEntity.java
@@ -0,0 +1,33 @@
+package cat.itacademy.barcelonactiva.Gamez.Luis.s05.t01.n02.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "flowers")
+@ToString
+@NoArgsConstructor
+public class FlowerEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter @Setter
+    private Integer pk_FlowerID;
+    @Column(name = "flowername")
+    @Getter @Setter
+    private String flowerName;
+    @Column(name = "branchcountry")
+    @Getter @Setter
+    private String flowerCountry;
+
+    public FlowerEntity(String flowerName, String flowerCountry) {
+
+        this.flowerName = flowerName;
+        this.flowerCountry = flowerCountry;
+    }
+}
