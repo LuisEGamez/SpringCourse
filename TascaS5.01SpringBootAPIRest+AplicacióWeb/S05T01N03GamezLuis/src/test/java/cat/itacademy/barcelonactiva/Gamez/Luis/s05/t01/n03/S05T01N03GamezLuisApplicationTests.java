b4138fb07a0550aa66a61e8e8a78dadd3415132f
LuisEGamez <luisgamezdvp@gmail.com>
2022-04-28 09:42:42 +0200

    First commit

---
+++ TascaS5.01SpringBootAPIRest+AplicacióWeb/S05T01N03GamezLuis/src/test/java/cat/itacademy/barcelonactiva/Gamez/Luis/s05/t01/n03/S05T01N03GamezLuisApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.Gamez.Luis.s05.t01.n03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S05T01N03GamezLuisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
