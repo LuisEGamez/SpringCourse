7b35344ad1008ee5ef5ec1976da8a31660741209
LuisEGamez <luisgamezdvp@gmail.com>
2022-05-16 11:21:44 +0200

    Delete classes

--- TascaS5.02DiceGame/dicegameMONGO/src/main/java/com/games/dicegame/model/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.games.dicegame.model.repository;
-
-import com.games.dicegame.model.domain.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-
-    Role findByName(String name);
-}
