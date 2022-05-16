9c52322f1dc3f61a619f191d43eb654878711498
LuisEGamez <luisgamezdvp@gmail.com>
2022-05-16 11:34:25 +0200

    Adding authorizations to Security confiuration class

--- TascaS5.02DiceGame/dicegame /src/main/java/com/games/dicegame/model/security/SecurityConfig.java
+++ TascaS5.02DiceGame/dicegame /src/main/java/com/games/dicegame/model/security/SecurityConfig.java
@@ -46,15 +46,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
-        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+        //customAuthenticationFilter.setFilterProcessesUrl("/api/login");
 
         http.csrf().disable(); // Disable a Cross Site Request Forgery
         http.sessionManagement().sessionCreationPolicy(STATELESS); // No session will be created or used by Spring Security.
         http.authorizeRequests().antMatchers("/login/**").permitAll();
+        http.authorizeRequests().antMatchers(POST,"/api/players").permitAll();
         http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
         http.addFilterAfter(new CustomAuthorizationFilter(userDetailsService), UsernamePasswordAuthenticationFilter.class); // We put the filter and the filter before of this.
         http.authorizeRequests().antMatchers(POST, "/api/players/{id}/games/**").access("@userSecurity.hasId(authentication,#id)"); // Give access t
-        http.authorizeRequests().antMatchers(DELETE, "/api/players/{id}/games/**").access("@userSecurity.hasId(authentication,#id)"); // Give access t
+        http.authorizeRequests().antMatchers(DELETE, "/api/players/{id}/games/**").access("@userSecurity.hasId(authentication,#id)");
+        http.authorizeRequests().antMatchers(GET, "/api/players/{id}/games/**").access("@userSecurity.hasId(authentication,#id)"); // Give access t
         http.authorizeRequests().antMatchers(POST, "/api/players").hasAnyAuthority("ROLE_ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
 
